51cdb41064400a6cbd80d3eb00a31ac3501cf350
Mahdi <mahdir798@gmail.com>
2026-06-15 16:18:45 +0330

    feat: integrate auth middleware and routes into main app

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
-
+import authRoutes from "./routes/authRoutes.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", authMiddleware, taskRoutes);
 
 export default app;
