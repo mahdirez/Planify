46e92faf9b1ed27ba373a602c17f41a756512a77
Mahdi <mahdir798@gmail.com>
2026-06-16 10:40:50 +0330

    feat: add auth token interceptor to axios client

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -5,3 +5,25 @@ export const API_URL = "http://localhost:5000";
 export const api = axios.create({
   baseURL: API_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+    }
+    return Promise.reject(error);
+  },
+);
