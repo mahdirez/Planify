2a9aaba66f1bcb40c031807a1968ee49c3c16c5b
mahdirezaei <mahdir798@gmail.com>
2026-08-13 18:04:19 +0330

    feat(error): add global error handler

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", authMiddleware, taskRoutes);
+
+app.use(errorHandler);
 
 export default app;
