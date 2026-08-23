0270503c67ac3bb64edaa6bab0b21bd10154bd1f
Mahdi <mahdir798@gmail.com>
2026-08-23 09:48:59 +0330

    feat(i18n): configure i18next with language detection

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
+import "./i18n";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
